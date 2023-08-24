d86a4a1b54a5c96bc6dbe39bc0f08828d20d1728
jequi1992 <jequirogal@ut.edu.co>
2023-08-23 22:42:13 -0500

    Ejercicios Clase 1

---
+++ Hallar perimetro y area.py
@@ -0,0 +1,10 @@
+# Hallar perimetro y area 
+
+a=15 #Asignación
+b=45 #Asignación
+perimetro=a*2+b*2 #operación del perimetro
+area=a*b #operación del area
+print(f'La Colombianada tiene {perimetro} mt de perimetro y {area} mt2 de area') # Mostrar por consola
+
+
+
