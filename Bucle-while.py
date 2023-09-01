ea68d9b57c0b247a54dd685b1096d69f82e650ec
jequi1992 <jequirogal@ut.edu.co>
2023-09-01 11:31:09 -0500

    Nuevo codigo

--- Bucle-while.py
+++ Bucle-while.py
@@ -3,10 +3,10 @@ print("=**SOLICITA NUMERO WHILE**=")
 print("======================================")
 contador=1
 numero=int(input("Ingrese un número --> "))
-while numero>10 or numero<0:
+while numero>0 or numero<10:
     numero=int(input("Ingrese un numero --> "))
     contador = contador +1
-    if(numero==5):
+    if(contador==5):
         print("Maximo numero de intentos alcanzados")
         break
 print("último Número ingresado: ", numero)
