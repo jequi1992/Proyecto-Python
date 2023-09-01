a5f2c0af2de74f2eb6d34d9c8167fd82ec992cf0
jequi1992 <jequirogal@ut.edu.co>
2023-09-01 10:32:05 -0500

    nuevos cambio

--- Dados.py
+++ Dados.py
@@ -8,7 +8,7 @@ cuentaseis=0
 for i in range(lanzamiento):
     resdado=random.randrange(1,7)
     print(f"Lanzamiento No. {i}:{resdado}")
-    if resdado==6:
+    if resdado==3:
         cuentaseis=cuentaseis+1
-print(f"{nom} En sus tres lanzamiento salieron {cuentaseis} seis")
+print(f"{nom} En sus {lanzamiento} lanzamiento(s) salieron {cuentaseis} seis")
 print("==============================================")
